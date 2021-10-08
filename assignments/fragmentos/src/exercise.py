2e6504a2017bb1c3f9d2df89737b441d0eac174f
JuannAraiza <a01740671@itesm.mx>
2021-10-08 14:07:45 +0000

    fragmentos

---
+++ assignments/fragmentos/src/exercise.py
@@ -0,0 +1,12 @@
+def main():
+    #escribe tu código abajo de esta línea
+    s = input('>>>')
+
+    print(len(s))
+    print(s[0])
+    print(s[-1])
+    print(s[1::2])
+        
+
+if __name__=='__main__':
+    main()
