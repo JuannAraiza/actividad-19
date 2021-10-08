848e3c21deee8ae355573f0970fc82fe825cc543
JuannAraiza <a01740671@itesm.mx>
2021-10-08 14:02:19 +0000

    longitud de palabras

---
+++ assignments/longitud/src/exercise.py
@@ -0,0 +1,16 @@
+def main():
+    #escribe tu código abajo de esta línea
+    s1= input('cadena 1: ')
+    s2= input('cadena 2: ')
+
+    if len(s1)==len(s2):
+        print(s1)
+        print(s2)
+    elif len(s1)>len(s2):
+        print(s1)
+    else:
+        print(s2)
+        
+
+if __name__=='__main__':
+    main()
